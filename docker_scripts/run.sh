3e623b65f532335acc502fa10de2c7d7dde56aba
Anton Sakhno <Anton_Sakhno@epam.com>
2019-08-16 14:34:21 +0300

    docker login for deploy stage

--- docker_scripts/run.sh
+++ docker_scripts/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+$(aws ecr get-login --region eu-west-2 --no-include-email)
 docker pull 101223776850.dkr.ecr.eu-west-2.amazonaws.com/aws-poc:latest
 docker run -dit --name aws-poc -p 8080:80 aws-poc
